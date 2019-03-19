ebcd91dffdd9ed9afcc6f315ea117de728b6fa3b
oMMiao <ommiao@foxmail.com>
2019-03-19 19:51:11 +0800

    音乐搜索结果UI微调。

--- app/src/main/java/cn/ommiao/musicmiao/adapter/SongListAdapter.java
+++ app/src/main/java/cn/ommiao/musicmiao/adapter/SongListAdapter.java
@@ -1,6 +1,9 @@
 package cn.ommiao.musicmiao.adapter;
 
 import android.support.annotation.Nullable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
 
 import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -10,6 +13,7 @@ import java.util.List;
 
 import cn.ommiao.musicmiao.R;
 import cn.ommiao.musicmiao.bean.Song;
+import cn.ommiao.musicmiao.utils.StringUtil;
 import cn.ommiao.musicmiao.widget.SquareImageView;
 
 public class SongListAdapter extends BaseQuickAdapter<Song, BaseViewHolder> {
@@ -20,13 +24,22 @@ public class SongListAdapter extends BaseQuickAdapter<Song, BaseViewHolder> {
 
     @Override
     protected void convert(BaseViewHolder helper, Song item) {
-        SquareImageView album = helper.getView(R.id.siv_music_album);
+        SquareImageView albumView = helper.getView(R.id.siv_music_album);
         Glide.with(mContext).load(item.getAlbumImageUrl())
                 .placeholder(R.drawable.pic_music_album)
                 .error(R.drawable.pic_music_album)
-                .into(album);
+                .into(albumView);
         helper.setText(R.id.tv_music_title, item.getTitle());
-        helper.setText(R.id.tv_music_singer, item.getOneSinger());
-        helper.setText(R.id.tv_music_album, item.getAlbum().getTitle());
+        String singer = StringUtil.isEmpty(item.getOneSinger()) ? mContext.getString(R.string.music_no_singer) : item.getOneSinger();
+        int singerLength = singer.length();
+        String sep = " / ";
+        int sepLength = sep.length();
+        String album = StringUtil.isEmpty(item.getAlbum().getTitle()) ? mContext.getString(R.string.music_no_album) : item.getAlbum().getTitle();
+        int albumLength = album.length();
+        String singerAndAlbum = singer + sep + album;
+        int singerAndAlbumLength = singerAndAlbum.length();
+        SpannableString singerAndAlbumSpan = new SpannableString(singerAndAlbum);
+        singerAndAlbumSpan.setSpan(new AbsoluteSizeSpan(13, true), singerLength + sepLength, singerAndAlbumLength, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        helper.setText(R.id.tv_music_singer_album, singerAndAlbumSpan);
     }
 }
